f23d2cce7a4c9c6090efd2fd08b90182a8e1137a
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2024-10-28 11:12:49 -0400

    commit

--- practice/intake.php
+++ practice/intake.php
@@ -1,6 +1,10 @@
 <?php
-ini_set(option: 'display_errors', value: 1);
-ini_set(option: 'display_startup_errors', value: 1);
-error_reporting(error_level: E_ALL);
 
-echo "PHP is working!";
+$animals = [
+    'Tiger',
+    'Lion',
+    'Hyena',
+    'Sloth'
+];
+
+require 'animals.view.php';
