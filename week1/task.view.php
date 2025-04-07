c4bbbffe1357dddb5981d25ef49a9bc56e32e76e
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2025-04-07 19:01:53 -0400

    week1 homework2

---
+++ week1/task.view.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+
+    <style>
+
+        body {
+
+            background:rgb(12, 12, 12);
+
+            padding: 2em;
+
+            text-align: left;
+
+            color: #ffffff;
+
+            font-family: Arial, Helvetica, sans-serif;
+
+        }
+
+        li {
+
+            padding: 1em;
+            
+        }
+
+    </style>
+
+</head>
+
+<body>
+    
+    <ul>
+
+        <?php foreach ($task as $descriptor => $entry) : ?>
+
+            <li><strong><?= $descriptor; ?></strong> <?= $entry; ?></li>
+
+        <?php endforeach; ?>    
+
+    </ul>
+
+</body>
+
+</html>
