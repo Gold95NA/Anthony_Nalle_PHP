a1d92f8f9089e7fbfdb3cff0c94d726d583d4b32
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2025-04-08 00:46:49 -0400

    Folders Organized for Week 1 Homework

---
+++ week1/Task_E/task_bool.php
@@ -0,0 +1,16 @@
+<?php
+
+$task = [
+
+    'Title: ' => 'PHP Homework',
+
+    'Assigned to: ' => 'Anthony Nalle',
+
+    'Due: ' => 'Tomorrow',
+
+    'Completed: ' => '❌'
+
+];
+
+
+require 'task_bool.view.php';
