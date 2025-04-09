1814db544142e6b4b11b6c901b390283215056ec
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2025-04-09 11:07:33 -0400

    work homepage still in progress

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+
+    <style>
+
+        body {
+
+            background:rgb(12, 12, 12);
+
+            padding: 2em;
+
+            text-align: left;
+
+            color: #ffffff;
+
+            font-family: Arial, Helvetica, sans-serif;
+
+        }
+
+        h1 {
+
+            display: flex;
+
+            justify-content: center;
+
+        }
+
+        .repo-link {
+
+            display: flex;
+
+            justify-content: center;
+
+        }
+
+        li {
+
+            padding: 1em;
+            
+        }
+
+        footer {
+
+            display: flex;
+
+            justify-content: center;
+
+        }
+
+    </style>
+
+</head>
+
+<body>
+    
+    <h1>Anthony Nalle PHP 266.06</h1>
+
+    <a class= "repo-link"><href>My Git Repo</href></a>
+    
+    <h2>PHP Resources</h2>
+
+    <ul>
+
+        <li></li>
+
+        <li></li>
+
+        <li></li>
+
+    </ul>
+
+    <h2>Git Resources</h2>
+
+    <ul>
+
+        <li></li>
+
+        <li></li>
+
+        <li></li>
+
+    </ul>
+
+    <h2>Assignments</h2>
+
+    <ul>
+        
+        <li>Week 3</li>
+
+        <li>Week 4</li>
+
+        <li>Week 5</li>
+
+        <li>Week 6</li>
+
+        <li>Week 7</li>
+
+        <li>Week 8</li>
+
+        <li>Week 9</li>
+
+        <li>Week 10</li>
+
+    </ul>
+
+    <h2>Hobbies</h2>
+
+    <ul>
+
+        <li></li>
+
+        <li></li>
+
+        <li></li>
+
+    </ul>
+
+    <footer>
+
+        <h3>Edited: XX/XX/XXXX at: XX:XXxm</h3>
+
+    </footer>
+
+</body>
+
+</html>
