e2614883c20c23cc0f7fe46c1729fad0f4a6b464
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2025-04-22 10:33:45 -0400

    week 2 homework finished

--- week2/Task_B/patient-intake.php
+++ week2/Task_B/patient-intake.php
@@ -1,5 +1,171 @@
 <?php
 
+function age($bdate) {
 
+    $date = new DateTime($bdate);
+
+    $now = new DateTime();
+
+    $interval = $now->diff($date);
+
+    return $interval->y;
+
+}
+
+function isDate($dt) {
+
+    $date_arr  = explode('-', $dt);
+
+    return count($date_arr) === 3 && checkdate($date_arr[1], $date_arr[2], $date_arr[0]);
+
+}
+
+function bmi($feet, $inches, $weight) {
+
+    $totalInches = $feet * 12 + $inches;
+
+    $heightMeters = $totalInches * 0.0254;
+
+    $weightKg = $weight / 2.20462;
+
+    return $weightKg / ($heightMeters * $heightMeters);
+
+}
+
+function bmiDescription($bmi) {
+    
+    if ($bmi < 18.5) return "Underweight";
+
+    elseif ($bmi < 25) return "Normal weight";
+
+    elseif ($bmi < 30) return "Overweight";
+
+    else return "Obese";
+}
+
+$errors = [];
+
+$submitted = $_SERVER["REQUEST_METHOD"] == "POST";
+
+$data = [
+
+    'first_name' => '',
+
+    'last_name' => '',
+
+    'married' => '',
+
+    'birth_date' => '',
+
+    'height_ft' => '',
+
+    'height_in' => '',
+
+    'weight' => '',
+
+];
+
+if ($submitted) {
+
+    foreach ($data as $key => $value) {
+
+        $data[$key] = trim($_POST[$key] ?? '');
+
+    }
+
+    if ($data['first_name'] === '') $errors['first_name'] = "First name is required.";
+
+    if ($data['last_name'] === '') $errors['last_name'] = "Last name is required.";
+
+    if ($data['married'] !== 'yes' && $data['married'] !== 'no') $errors['married'] = "Please select if married.";
+
+    if (!isDate($data['birth_date'])) $errors['birth_date'] = "Invalid birth date.";
+
+    if (!is_numeric($data['height_ft']) || $data['height_ft'] < 1 || $data['height_ft'] > 8) $errors['height_ft'] = "Invalid height (feet).";
+
+    if (!is_numeric($data['height_in']) || $data['height_in'] < 0 || $data['height_in'] >= 12) $errors['height_in'] = "Invalid height (inches).";
+
+    if (!is_numeric($data['weight']) || $data['weight'] < 1 || $data['weight'] > 1000) $errors['weight'] = "Invalid weight.";
+
+}
+
+if ($submitted && empty($errors)) {
+
+    $age = age($data['birth_date']);
+
+    $bmiValue = bmi($data['height_ft'], $data['height_in'], $data['weight']);
+
+    $bmiRounded = round($bmiValue, 1);
+
+    $bmiDesc = bmiDescription($bmiValue);
+
+    echo "<style>body {
+
+            background:rgb(12, 12, 12);
+
+            padding: 2em;
+
+            text-align: left;
+
+            color: #ffffff;
+
+            font-family: Arial, Helvetica, sans-serif;
+
+        }
+
+        h1 {
+
+            display: flex;
+
+            justify-content: center;
+
+            padding-bottom: 40px;
+
+        }
+
+        form {
+
+            display: flex;
+
+            flex-direction: column;
+
+            justify-content: center;
+
+        }
+
+        strong {
+
+            padding-right: 10px;
+
+        }
+
+        p {
+
+            margin-left: 550px;
+
+            padding-bottom: 40px;
+
+        }
+            
+        }</style>";
+
+    echo "<h1>Patient Information</h1>";
+
+    echo "<p><strong>Name:</strong> {$data['first_name']} {$data['last_name']}</p>";
+
+    echo "<p><strong>Married:</strong> " . ucfirst($data['married']) . "</p>";
+
+    echo "<p><strong>Birth Date:</strong> {$data['birth_date']} (Age: $age)</p>";
+
+    echo "<p><strong>Height:</strong> {$data['height_ft']}' {$data['height_in']}\"</p>";
+
+    echo "<p><strong>Weight:</strong> {$data['weight']} lbs</p>";
+    
+    echo "<p><strong>BMI:</strong> $bmiRounded ($bmiDesc)</p>";
+    
+    exit;
+
+}
 
 require 'patient-intake.view.php';
+
--- week2/Task_B/patient-intake.view.php
+++ week2/Task_B/patient-intake.view.php
@@ -29,31 +29,136 @@
 
             justify-content: center;
 
+            padding-bottom: 40px;
+
         }
 
-        li {
+        form {
 
-            padding: 1em;
+            display: flex;
+
+            flex-direction: column;
+
+            justify-content: center;
             
+        }
+
+        label {
+
+            margin-left: 550px;
+        }
+
+        .sub-btn {
+
+            width: 100px;
+
+            margin-left: 650px;
+
+        }
+
+        .marriage-check {
+
+            margin-left: 25px;
+
+        }
+
+        .bday-check {
+
+            margin-left: 8px;
+
+        }
+
+        .height-input {
+
+            margin-left: 34px;
+
         }
 
     </style>
 
 </head>
 
+
+
 <body>
 
     <h1>Patient Intake Form</h1>
 
-    <ul>
+    <form method="post">
 
-        <li></li>
+        <label>First Name:
 
-        <li></li>
+            <input type="text" name="first_name" value="<?= htmlspecialchars($data['first_name']) ?>">
 
-        <li></li>
+            <span style="color:red"><?= $errors['first_name'] ?? '' ?></span>
 
-    </ul>
+        </label>
+        
+        <br><br>
+
+        <label>Last Name:
+
+            <input type="text" name="last_name" value="<?= htmlspecialchars($data['last_name']) ?>">
+
+            <span style="color:red"><?= $errors['last_name'] ?? '' ?></span>
+            
+        </label>
+        
+        <br><br>
+
+        <label>Married:
+
+            <select class="marriage-check" name="married">
+
+                <option value="">--Select--</option>
+
+                <option value="yes" <?= $data['married'] == 'yes' ? 'selected' : '' ?>>Yes</option>
+
+                <option value="no" <?= $data['married'] == 'no' ? 'selected' : '' ?>>No</option>
+
+            </select>
+
+            <span style="color:red"><?= $errors['married'] ?? '' ?></span>
+
+        </label>
+        
+        <br><br>
+
+        <label>Birth Date:
+
+            <input class="bday-check" type="date" name="birth_date" value="<?= htmlspecialchars($data['birth_date']) ?>">
+
+            <span style="color:red"><?= $errors['birth_date'] ?? '' ?></span>
+
+        </label>
+        
+        <br><br>
+
+        <label>Height:
+
+            <input class="height-input" type="number" name="height_ft" placeholder="ft" min="1" max="8" value="<?= htmlspecialchars($data['height_ft']) ?>"> ft
+
+            <input type="number" name="height_in" placeholder="in" min="0" max="11" value="<?= htmlspecialchars($data['height_in']) ?>"> in
+
+            <span style="color:red"><?= $errors['height_ft'] ?? '' ?> <?= $errors['height_in'] ?? '' ?></span>
+
+        </label>
+        
+        <br><br>
+
+        <label>Weight (lbs):
+
+            <input type="number" name="weight" value="<?= htmlspecialchars($data['weight']) ?>">
+
+            <span style="color:red"><?= $errors['weight'] ?? '' ?></span>
+
+        </label>
+        
+        <br><br>
+
+        <input class="sub-btn" type="submit" value="Submit">
+
+    </form>
 
 </body>
 
